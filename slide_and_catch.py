1f80dba98c4e16f86291450499fb3636f89be478
Langnir1 <joshua.halamka@bsu.edu>
2024-10-31 12:37:53 -0400

    Start of code
    The code doesn't run due to self.sprites[] having invalid syntax

---
+++ slide_and_catch.py
@@ -0,0 +1,18 @@
+import simpleGE, pygame, random
+
+
+class Game(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setImage("cosmos.jpg")
+        
+        
+        self.sprites[]
+        
+def main():
+    game = Game()
+    game.start()
+    
+if __name__ == "__main__":
+    main()
+    
