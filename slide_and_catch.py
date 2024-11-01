99d197cf72a6235ac79bf42c643f51f2b3967a67
Langnir1 <joshua.halamka@bsu.edu>
2024-11-01 10:41:59 -0400

    Player image can move
    This update works and lets the player image move left and right according to which arrow key is pressed

--- slide_and_catch.py
+++ slide_and_catch.py
@@ -1,13 +1,26 @@
 import simpleGE, pygame, random
 
+class Sword(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("fire_sword.png")
+        self.setSize(50, 70)
+        self.position = (300, 400)
+        self.moveSpeed = 5
 
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+            
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("cosmos.jpg")
+        self.sword = Sword(self)        
         
-        
-        self.sprites[]
+        self.sprites = [self.sword]
         
 def main():
     game = Game()
